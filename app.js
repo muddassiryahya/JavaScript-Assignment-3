35641328bdfac9f78307cd1de940ba66629aecb6
muddassiryahya <m.m2442212@gmail.com>
2023-11-25 18:23:07 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// var color = prompt("Enter a color name:")
+ 
+// if (color == "red") {
+//     swal({
+//         title: "must stop!",
+//         text: "You clicked the button!",
+//         icon: "success",
+//         button: "Aww yiss!",
+//       });
+// }
+// else if (color === "yellow") {
+//     swal("ready to move!", "You clicked the button!", "success", {
+//         button: "Aww yiss!",
+//       });
+// }
+// else if (color === "green") {
+//     swal({
+//         title: "move on!",
+//         text: "You clicked the button!",
+//         icon: "success",
+//       });
+// }
+// else {
+    // swal("collection wrong")
+// }
+
+
